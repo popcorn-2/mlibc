08fb3a3aaf13d67b0bb12428916ddbbcef531afd
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-12-15 18:06:06 +0000

    bump popcorn to match new abi-bits

--- abis/popcorn/limits.h
+++ abis/popcorn/limits.h
@@ -1,14 +1,13 @@
 #ifndef _ABIBITS_LIMITS_H
 #define _ABIBITS_LIMITS_H
 
-#define IOV_MAX 1024
-
-/* Default process priority */
-#define NZERO 20
+#define __MLIBC_IOV_MAX 1024
+#define __MLIBC_LOGIN_NAME_MAX 256
 
 /* Maximum hostname length, posix defines it as 255 */
-#define HOST_NAME_MAX 255
+#define __MLIBC_HOST_NAME_MAX 255
 
-#define OPEN_MAX 256
+#define __MLIBC_OPEN_MAX 256
+#define __MLIBC_NAME_MAX 255
 
 #endif /*_ABIBITS_LIMITS_H */
